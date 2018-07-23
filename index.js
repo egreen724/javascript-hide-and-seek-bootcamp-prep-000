7f0c68a39a6d6f578f6671ffc148439077b1a182
Erin Green <erin.y.green@gmail.com>
2018-07-23 20:15:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+} 
 
+function nestedTarget() {
+  return document.querySelector('div.target')
+}
+
+function increaseRankBy(n) {
+  var listString = document.querySelectorAll("ul.ranked-list li");
+  
+  for (let i = 0; i < listString.length; i++) {
+    listString[i].innerHTML = ((parseInt(listString[i].innerHTML)) + n)
+  }
+}
+
+function deepestChild() {
+  return document.querySelector('div#grand-node div div div div')
+}
