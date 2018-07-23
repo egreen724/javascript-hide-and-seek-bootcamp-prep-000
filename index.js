177f68a6866afc47176d6a54634bafd2a3bc9876
Erin Green <erin.y.green@gmail.com>
2018-07-23 20:16:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
 } 
 
 function nestedTarget() {
-  return document.querySelector('div.target')
+  return document.querySelector('#nested.target')
 }
 
 function increaseRankBy(n) {
